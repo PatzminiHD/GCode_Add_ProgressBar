6e60ec7d18b53150d4b7205e4ac9576efd7383a7
PatzminiHD-Win10 <7.0@gmx.at>
2022-03-13 12:10:07 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+namespace GCode_Add_ProgressBar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sFilePath;
+            try
+            {
+                sFilePath = args[0];
+            }
+            catch
+            {
+                WriteLine("Please Input the Filepath:");
+                sFilePath = ReadLine();
+            }
+            if(File.Exists(sFilePath))
+            {
+                string[] saFile = File.ReadAllLines(sFilePath);
+                int iPercent;
+                for (int i = 0; i <= 100; i++)
+                {
+                    
+                }
+            }
+            else
+            {
+                ForegroundColor = ConsoleColor.Red;
+                WriteLine($"The specified File \"{sFilePath}\" does not exist.");
+                ResetColor();
+            }
+        }
+        public static void AddNewLineToArray(int iYPos, ref string[] saFile, string sStringToAdd)
+        {
+            string[] saOut = new string[saFile.Length + 1];
+            for (int i = 0; i < iYPos; i++)
+            {
+                saOut[i] = saFile[i];
+            }
+            saOut[iYPos] = saFile[iYPos];
+            saOut[iYPos + 1] = sStringToAdd;
+
+            for (int i = iYPos + 2; i < saOut.Length; i++)
+            {
+                saOut[i] = saFile[i - 1];
+            }
+            saFile = saOut;
+        }
+    }
+}
